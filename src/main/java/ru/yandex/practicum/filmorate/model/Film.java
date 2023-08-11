2132d36c3c4115853188edc1b84dfa06b9e409fa
oldixi <oldixi@yandex.ru>
2023-08-11 08:47:41 +0300

    refactor: убраны неиспользуемые imports.

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -2,7 +2,6 @@ package ru.yandex.practicum.filmorate.model;
 
 import lombok.Builder;
 import lombok.Data;
-import ru.yandex.practicum.filmorate.controller.ValidationException;
 
 import java.time.LocalDate;
 
--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -2,7 +2,6 @@ package ru.yandex.practicum.filmorate.model;
 
 import lombok.Builder;
 import lombok.Data;
-import ru.yandex.practicum.filmorate.controller.ValidationException;
 
 import javax.validation.constraints.Email;
 import java.time.LocalDate;
