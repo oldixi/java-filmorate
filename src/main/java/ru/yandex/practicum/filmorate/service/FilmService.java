bdc978b82000d53fd9be7882220b239806abd1a9
oldixi <oldixi@yandex.ru>
2023-10-16 14:34:00 +0300

    style: delete not used importsrefactor

--- src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
@@ -11,7 +11,6 @@ import ru.yandex.practicum.filmorate.storage.LikeStorage;
 import ru.yandex.practicum.filmorate.storage.FeedStorage;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
